28498fdd343ef6a461af428c5a81690a32517479
preyesh2002 <119518427+preyesh2002@users.noreply.github.com>
2024-12-11 20:13:46 +0530

    Update Auth.tsx

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,6 +1,6 @@
 import { Link , useNavigate} from "react-router-dom";
 import { ChangeEvent, useState } from "react";
-//import { SingupInput } from "@preyeshcp/medium-common";
+import { SingupInput } from "@preyeshcp/medium-common";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
 
@@ -8,7 +8,7 @@ import { BACKEND_URL } from "../config";
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
     const navigate=useNavigate()
-    const [postInputs,setPostInputs]=useState({
+    const [postInputs,setPostInputs]=useState<SingupInput>({
         // name:"",
         email:"",
         password:""
